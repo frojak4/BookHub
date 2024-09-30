6acdb41c40343708e11338883fd0f7f57854adbc
frojak4 <Frojak4@gmail.com>
2024-09-30 13:01:28 +0200

    Added password input

--- frontend/bookhub/src/pages/LogIn.jsx
+++ frontend/bookhub/src/pages/LogIn.jsx
@@ -8,13 +8,20 @@ const LogIn = () => {
 
     const [register, setRegister] = useState(false);
     const [usernameInput, setUsernameInput] = useState("");
+    const [passwordInput, setPasswordInput] = useState("");
     const [errorMSG, setErrorMSG] = useState(false);
     const [user, setUser] = useContext(userContext);
     const navigate = useNavigate();
 
 
-    const handleChange = (e) => {
+    const handleUserChange = (e) => {
         setUsernameInput(e.target.value)
+        errorMSG(false);
+    }
+
+    const handlePasswordChange = (e) => {
+        setPasswordInput(e.target.value)
+        errorMSG(false);
     }
 
     useEffect(() => {
@@ -36,30 +43,30 @@ const LogIn = () => {
         <div className="text-white flex justify-center mt-8">Welcome to BookHub, please log in or register to continue</div>
         <div>
             <div className="flex justify-center mt-4">
-                <button onClick={() => setRegister(!register)} className="bg-white disabled:bg-slate-600 mx-1 w-16 rounded-lg" disabled={!register}>
+                <button onClick={() => setRegister(!register)} className="bg-white disabled:bg-slate-600 mx-1 w-20 rounded-lg py-1" disabled={!register}>
                     Login
                 </button>
-                <button onClick={() => setRegister(!register)} className="bg-white disabled:bg-slate-600 mx-1 w-16 rounded-lg" disabled={register}>
+                <button onClick={() => setRegister(!register)} className="bg-white disabled:bg-slate-600 mx-1 w-20 rounded-lg py-1" disabled={register}>
                     Register
                 </button>
             </div>
-            {register ? 
-                <div>
-                    <div className="flex mt-24 justify-center">
-                    <input onChange={handleChange} value={usernameInput} className="mr-3 h-8 p-2" type="text" placeholder="Username"/> 
-                    <button className="bg-slate-600 text-white ml-3 w-18 px-2 h-8"> 
-                        Register
+                
+                <div className="flex mt-2 justify-center flex-col w-56 mx-auto items-center bg-gray-800 p-6 rounded-xl">
+                    <input onChange={handleUserChange} value={usernameInput} className="h-8 p-2 my-1" type="text" placeholder="Username"/> 
+                    <input onChange={handlePasswordChange} value={passwordInput} className="h-8 p-2 my-1" type="password" placeholder="Password"/> 
+                    {register ? 
+                    <button onClick={handleLogIn} className="bg-slate-600 text-white my-1 w-24 h-8 px-2 hover:cursor-pointer"> 
+                    Register
                     </button>
-                </div>
-                </div> : 
-                <div className="flex mt-24 justify-center">
-                    <input onChange={handleChange} value={usernameInput} className="mr-3 h-8 p-2" type="text" placeholder="Username"/> 
-                    <button onClick={handleLogIn} className="bg-slate-600 text-white ml-3 w-18 h-8 px-2 hover:cursor-pointer"> 
+                    :
+                    <button onClick={handleLogIn} className="bg-slate-600 text-white my-1 w-24 h-8 px-2 hover:cursor-pointer"> 
                         Log In
                     </button>
+                    }
+                    
                 </div>
-            }
-            {errorMSG && <h3 className="text-red-500">Could not log in to account</h3>}
+                {errorMSG && <h3 className="text-red-500 text-center">Could not log in to account</h3>}
+            
         </div>
     </div>
   )
